6a2d23befc02bc1fc8547a186ac36cd0758ad544
ClashMania <113007885+ClashMania@users.noreply.github.com>
2023-05-10 14:47:18 +0200

    Add files via upload

---
+++ Karte/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karte
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int karta = 0, zbroj = 0;
+            do
+            {
+                Console.WriteLine("Unesi broj od 1 do 13: ");
+                karta = Convert.ToInt32(Console.ReadLine());
+                if (karta >= 1 && karta <= 13)
+                {
+                    zbroj += karta;
+                }
+            }
+            while (zbroj < 31);
+            if (zbroj == 31)
+            {
+                Console.WriteLine("Pobjedili ste");
+            }
+            else
+            {
+                Console.WriteLine("Izgubili ste");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
